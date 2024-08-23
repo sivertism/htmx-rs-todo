dcf1a37122adf5b587b0542b6bf33c6e35e51437
sivertism <10866270+sivertism@users.noreply.github.com>
2024-08-23 16:13:41 +0200

    Basic TLS integration done. Hardcoded certs

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,57 @@ mod template;
 mod todo;
 mod database;
 
-use axum::extract::Path;
-use axum::Form;
-use axum::response::IntoResponse;
-use axum::routing::{delete, get, post};
-use axum::http::StatusCode;
-use tokio::net::TcpListener;
+#[allow(unused_imports)]
+use axum::{
+    Form,
+    extract::Path,
+    response::{IntoResponse, Redirect},
+    routing::{delete, get, post},
+    http::{StatusCode, Uri},
+    BoxError,
+    Router
+};
+use axum_server::tls_rustls::RustlsConfig;
+use std::{net::SocketAddr, path::PathBuf};
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 use todo::{Todo,TodoForm};
 use template::{HtmlTemplate, IndexTemplate, TodosTemplate, TodosCompleteTemplate};
 
+#[allow(dead_code)]
+#[derive(Clone, Copy)]
+struct Ports {
+    http: u16,
+    https: u16,
+}
+
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
+    
+    tracing_subscriber::registry()
+    .with(
+        tracing_subscriber::EnvFilter::try_from_default_env()
+            .unwrap_or_else(|_| "example_tls_rustls=debug".into()),
+    )
+    .with(tracing_subscriber::fmt::layer())
+    .init();
+
+    let ports = Ports {
+        http: 7878,
+        https: 3000,
+    };
+
+    // configure certificate and private key used by https
+    // #TODO use options to set path to certs
+    let config = RustlsConfig::from_pem_file(
+        PathBuf::from(env!("CARGO_MANIFEST_DIR"))
+            .join("self_signed_certs")
+            .join("cert.pem"),
+        PathBuf::from(env!("CARGO_MANIFEST_DIR"))
+            .join("self_signed_certs")
+            .join("key.pem"),
+    )
+    .await
+    .unwrap();
 
     let app = axum::Router::new() // Create a new Axum Router
             .route("/", get(index)) // Define a GET route for the root path, handled by the
@@ -22,12 +62,13 @@ async fn main() -> std::io::Result<()> {
             // :id defines path parameters for our route
             .route("/todo/:id", delete(delete_todo).post(toggle_todo));
 
-    // Bind a TCP listener to the specified address
-    let listener = TcpListener::bind("0.0.0.0:3000").await?;
-    println!("listening on {}", listener.local_addr().unwrap());
-
-    // Start the Axum server with the defined routes
-    axum::serve(listener, app).await?;
+    // Run https server
+    let addr = SocketAddr::from(([0,0,0,0], ports.https));
+    tracing::debug!("Listening on {}", addr);
+    println!("Listening on {}", addr);
+    axum_server::bind_rustls(addr, config)
+      .serve(app.into_make_service())
+      .await?;
     Ok(())
 }
 
