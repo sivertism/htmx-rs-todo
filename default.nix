c7062c595f012b5c4ac31dab9c1bf749195c2fdf
sivertism <10866270+sivertism@users.noreply.github.com>
2024-08-24 14:12:42 +0200

    Fixed pname in default.nix

--- default.nix
+++ default.nix
@@ -1,7 +1,7 @@
 { pkgs ? import <nixpkgs> { } }:
 with pkgs;
 rustPlatform.buildRustPackage rec {
-  pname = "htmx-todo";
+  pname = "htmx-rs-todo";
   version = "0.1.0";
   cargoLock.lockFile = ./Cargo.lock;
   src = pkgs.lib.cleanSource ./.;
