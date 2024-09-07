c758e5e648c9d1cb488b62fb5e1b1fd6842cd68d
sivertism <10866270+sivertism@users.noreply.github.com>
2024-09-07 21:02:57 +0200

    Fixed flake.nix

--- default.nix
+++
@@ -1,9 +0,0 @@
-{ pkgs ? import <nixpkgs> { } }:
-with pkgs;
-rustPlatform.buildRustPackage rec {
-  pname = "htmx-rs-todo";
-  version = "0.1.0";
-  cargoLock.lockFile = ./Cargo.lock;
-  src = pkgs.lib.cleanSource ./.;
-}
-
